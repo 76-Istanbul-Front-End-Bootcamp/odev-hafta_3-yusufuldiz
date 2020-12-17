bf875f8d41ae4d39ebc00b71278b1bcff7cc495e
Yusuf Yıldız <48319174+yusufuldiz@users.noreply.github.com>
2020-12-17 18:28:17 +0000

    class cevrilme islemi yapildi

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
+class AnimalClass {
+
+constructor(name){
+this.name = name;
+}
+
+action(){
+  document.getElementById(this.actionSoundName).play();
+}
+
+putInTheDocument(){
+    var petsTable = document.getElementById("petsTable");
+    var petTR = document.createElement("tr");
+  
+    var petNameTD = document.createElement("td");
+    petNameTD.textContent = this.name;
+    petTR.appendChild(petNameTD);
+  
+    var petLegsTD = document.createElement("td");
+    petLegsTD.textContent = this.legs;
+    petTR.appendChild(petLegsTD);
+  
+    var petActionTD = document.createElement("td");
+    var petActionTDButton = document.createElement("button");
+    petActionTDButton.textContent = this.actionText;
+    petActionTD.appendChild(petActionTDButton);
+    petTR.appendChild(petActionTD);
+    
+    petActionTDButton.onclick = this.action.bind(this);
+    petsTable.querySelector("tbody").appendChild(petTR)
+}
+
+}
+
+class Cat extends AnimalClass{
+    constructor(name){
+        super(name);
+        this.legs = 4;
+        this.actionText = "Meoow"
+        this.actionSoundName = "meow"
+    }
+}
+
+class Monkey extends AnimalClass{
+    constructor(name){
+        super(name);
+        this.legs = 2;
+        this.actionText = "Scream";
+        this.actionSoundName = "scream";
+    }
+}
+
+var Mila = new Cat("Mila");
+Mila.putInTheDocument();
+
+var Charlie = new Monkey("Charlie");
+Charlie.putInTheDocument();
+
+
+
+
+
+
+/*
 function Animal(name){
     this.name = name;
 }
@@ -52,4 +116,4 @@ var Mila = new Cat("Mila");
 Mila.putInTheDocument();
 
 var Charlie = new Monkey("Charlie");
-Charlie.putInTheDocument();
+Charlie.putInTheDocument(); */
